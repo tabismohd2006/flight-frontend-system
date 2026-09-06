c9d4a1e849b18ba00fa0e61767c0092c88fb4bb5
mohd tabis <tabismohd676@gmail.com>
2026-09-06 16:53:27 +0530

    Fix PopularDestinations casing

--- src/pages/public/Home.jsx
+++ src/pages/public/Home.jsx
@@ -1,5 +1,5 @@
 import Hero from "../../components/home/Hero";
-import PopularDestinations from "../../components/home/popularDestinations";
+import PopularDestinations from "../../components/home/PopularDestinations";
 import Features from "../../components/home/Features";
 import Offers from "../../components/home/Offers";
 import Airlines from "../../components/home/Airlines";
